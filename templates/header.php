13b9f1ce3fd02f2ec9a3b564d47036b21d7cc30b
Samuel Torimiro <efosonaname@gmail.com>
2023-10-13 16:24:54 -0500

    minor edits

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@
      <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/style.css">
 
     
     
